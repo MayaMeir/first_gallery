3c965fbb09dd9061c8a3cb14dd4ee56af0a3f193
MayaMeir <76560531+MayaMeir@users.noreply.github.com>
2021-01-04 14:17:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,25 +4,28 @@ var new_pic = document.querySelector("#image_url");
 var bigImgFig = document.querySelector("#enlarged_img");
 var shownPics = [];
 let images = [
-    { src: "UFO.jpg", title: 'UFO', numbers: '3'},
-    { src: "space.jpg", title: 'THE SPACE', numbers: '1' },
-    { src: "nebula.jpg", title: 'NEBULA', numbers: '2' }
+    { src: "images/UFO.jpg", title: 'UFO', numbers: '3', id:'0'},
+    { src: "images/space.jpg", title: 'THE SPACE', numbers: '1', id:'1'},
+    { src: "images/nebula.jpg", title: 'NEBULA', numbers: '2', id:'2'}
 ];
 let index = 0;
 let gallery = document.querySelector("#gallery");
 let title = document.querySelector("#caption");
 let numberInput = document.querySelector("#numbers");
+var items = 3;
+let id;
 
 
 
 function addPic(e) {
     e.preventDefault();
     if (new_pic.value && title.value && numberInput.value) {
-        images.push({ src: new_pic.value, title: title.value, numbers: numberInput.value});
+        images.push({ src: new_pic.value, title: title.value, numbers: numberInput.value, id:items});
         new_pic.value = '';
         title.value = '';
         numberInput.value = '';
-
+        localStorage.setItem('myGallery', JSON.stringify(images));
+        items++;
     }
 }
 
@@ -40,12 +43,18 @@ function enlargeImg(img) {
     bigImgFig.innerHTML = `<img src="${src}" onclick="reset()">`;
 }
 
-function Delete(fig){
+function Delete(fig) {
+    console.log(fig.id);
+    let arrindex = images.findIndex((obj) =>{ return obj.id === fig.parentNode.parentNode.id });
+    console.log(arrindex);
+    images.splice(arrindex, 1);
     fig.parentNode.parentNode.remove();
+    localStorage.setItem('myGallery', JSON.stringify(images));
 }
 
 function toImgItem(image) {
-    return `<figure>
+    id = image.id;
+    return `<figure id=${id}>
     <img src="${image.src}" alt ="${image.src}" onclick="enlargeImg(this)">
     <figcaption>Title: ${image.title} || Distance: ${image.numbers} || <button id="delbtn" onclick="Delete(this)">X</button></figcaption>
     </figure>`;
@@ -59,7 +68,6 @@ function setDisplayImages() {
     for (let i = index; i < images.length && counter < 3; i++, counter++) {
         shownPics.push(images[i]);
     }
-    // shownPics.sort(compareByNum);
     return shownPics.map(toImgItem);
 }
 
@@ -69,7 +77,6 @@ function render() {
 }
 
 function showNext3() {
-
     if (index + 3 < images.length) {
         index = index + 3;
         render();
@@ -78,12 +85,16 @@ function showNext3() {
 }
 
 function showPrev3() {
-
     if (index - 3 >= 0) {
         index = index - 3;
         render();
     }
 }
 
-render();
+window.onload = function () {
+    if (JSON.parse(localStorage.getItem('myGallery'))) {
+        images = JSON.parse(localStorage.getItem('myGallery'));
+    }
+    render();
+}
 
